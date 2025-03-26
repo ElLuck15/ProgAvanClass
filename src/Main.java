48c0f0f4715dd16a80cb62e9f88a5aa7c5e5a0bc
ElLuck <sebitaluck2015@gmail.com>
2025-03-26 11:59:08 -0300

    DevelopFunc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Main {
+    static Random rd = new Random();
+    static List<Float> notas = new ArrayList<Float>();
+    static List<Float> notasmenortres = new ArrayList<Float>();
+    static List<Float> notasmay = new ArrayList<Float>();
+    static List<Float> notasbetw = new ArrayList<Float>();
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        CrearNotas();
+        ProcesarNotasFunc();
+        SalidaFunc();
+    }
+    public static void CrearNotas(){
+        IntStream.range(0,rd.nextInt(5,10)).forEach(i->notas.add(rd.nextFloat(1.0f,5.0f)));
+    }
+    public static void ProcesarNotasFunc(){
+        notas.stream().filter(i->i<3.6f).forEach(i->notasmenortres.add(i));
+        notas.stream().filter(i->i>4f).forEach(i->notasmay.add(i));
+        notas.stream().filter(i->i<=4f&&i>=3.6).forEach(i->notasbetw.add(i));
+    }
+    public static void SalidaFunc(){
+        System.out.println("Todas las notas");
+        notas.stream().forEach(i->System.out.println(i));
+        System.out.println("Notas menores a 3.6");
+        notasmenortres.stream().forEach(i->System.out.println(i));
+        System.out.println("Notas mayores a 4");
+        notasmay.stream().forEach(i->System.out.println(i));
+        System.out.println("Notas entre 3.6 y 4");
+        notasbetw.stream().forEach(i->System.out.println(i));
     }
 }
